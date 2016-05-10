90aba146e63d71b70f6a87253e00c6cadff25388
elana tee <elanatee@gmail.com>
2016-05-09 23:47:31 -0400

    redoing scales.py

---
+++ scales.py
@@ -0,0 +1,13 @@
+from getKey import *
+
+def main():
+	getScales()
+
+def getScales():
+	scaleType = input('which scale would you like? 1) major, 2) minor, 3) minor bebop\n')
+	if scaleType == 1:
+		getKey()
+		print currentKey
+
+if __name__ == '__main__':
+	main()
